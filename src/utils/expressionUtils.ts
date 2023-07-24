de8305e797be7446c14b838f6d7ddd3e9d161b26
martin <me@martials.no>
2023-07-24 22:21:43 +0200

    Refactor replaceOperators function to use chain methods

--- src/utils/expressionUtils.ts
+++ src/utils/expressionUtils.ts
@@ -4,13 +4,11 @@
  * @returns The expression with the replaced operators
  */
 export function replaceOperators(expression: string): string {
-
-    expression = expression.replaceAll(/\//g, "|");
-    expression = expression.replaceAll(/¬/g, "!");
-    expression = expression.replaceAll(/\sOR\s/gi, " | ");
-    expression = expression.replaceAll(/\sAND\s/gi, " & ");
-    expression = expression.replaceAll(/\s(IMPLICATION|IMP)\s/gi, " -> ");
-    expression = expression.replaceAll(/\sNOT\s/gi, " !");
-
-    return expression;
+    return expression
+        .replaceAll(/\//g, "|")
+        .replaceAll(/¬/g, "!")
+        .replaceAll(/\sOR\s/gi, " | ")
+        .replaceAll(/\sAND\s/gi, " & ")
+        .replaceAll(/\s(IMPLICATION|IMP)\s/gi, " -> ")
+        .replaceAll(/\sNOT\s/gi, " !");
 }
