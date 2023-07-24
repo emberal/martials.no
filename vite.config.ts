7656d3522751cb0cd4aa347a749420fc3b3c888a
martin <me@martials.no>
2023-07-24 20:47:55 +0200

    Changed the main entry file in Vite's rollupOptions from 'app.html' to 'index.html'.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'esnext',
         rollupOptions: {
             input: {
-                main: "app.html",
+                main: "index.html",
             }
         }
     },
